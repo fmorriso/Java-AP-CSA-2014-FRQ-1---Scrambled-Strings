8a3e4a5292f7033c2b05339be0194161953b3557
Fred Morrison <frederick.morrison@outlook.com>
2024-11-13 11:34:34 -0700

    surround failing student code with try/catch

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,11 @@ public class Main {
                         "TAN", "ABRACADABRA", "WHOA", "AARDVARK", "EGGS", "A", ""
                 };
 
-/*        for (String word : words) {
-            System.out.println(word + " becomes " + scrambleWordSolution(word));
-        }*/
+        for (String word : words) {
+            System.out.println("Working solution: '" + word + "' becomes 1" + scrambleWordSolution(word) + "'");
+            System.out.format("Student solution: '%s' becomes '%s'%n", word, scrambleWordStudent(word));
+        }
 
-        String s = scrambleWordSolution("TAN");
-        System.out.println(s);
-
-        s = scrambleWordStudent("TAN");
-        System.out.println(s);
     }
 
     public static String scrambleWordWhileLoop(String word) {
@@ -28,22 +24,34 @@ public class Main {
     }
 
     public static String scrambleWordStudent(String word) {
+        System.out.format("Student solution: attempting to scramble word: '%s'%n", word);
         /* to be implemented in part a */
         String finword = word;
         int digit = -1;
         while (digit < word.length()) {
             digit++;
             if ((word.indexOf("A") == digit) && ((digit + 1) <= word.length())) {
-                word = word.substring(0, digit - 1) + "a" + word.substring(digit + 1);
+                try {
+                    word = word.substring(0, digit - 1) + "a" + word.substring(digit + 1);
+                } catch (Exception e) {
+                    System.err.format("1. Student code failed: %s%n", e.getMessage());
+                    break;
+                }
+
+                try {
                 finword = word.substring(0, digit - 1) + word.substring(digit + 1);
+                } catch (Exception e) {
+                    System.err.format("2. Student code failed: %s%n", e.getMessage());
+                    break;
+                }
+
                 digit++;
                 try {
                     finword = word.substring(0, digit - 1) + "A" + word.substring(digit + 1);
                 } catch (Exception e) {
-                    System.out.format("Student code failed: %s%n",e.getMessage());
+                    System.err.format("3. Student code failed: %s%n", e.getMessage());
                     break;
                 }
-
             }
         }
         return finword;
